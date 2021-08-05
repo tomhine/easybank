df151f156b57b7437880d342ba878086d78fc725
Tom Hine <tomhine792@gmail.com>
2021-08-05 13:04:02 +0100

    added info section and cards with data

---
+++ components/InfoSection/info-data.js
@@ -0,0 +1,36 @@
+const info = [
+  {
+    id: 0,
+    alt: "hand holding bank cards",
+    image: "/images/icon-online.svg",
+    title: "Online Banking",
+    message:
+      "Our modern web and mobile applicatons allow you to keep track of your finances wherever you are in the world.",
+  },
+  {
+    id: 1,
+    alt: "app with dollar sign logo",
+    image: "/images/icon-budgeting.svg",
+    title: "Simple Budgeting",
+    message:
+      "See exactly where your money goes each month. Receive notificatoins when you're close to hitting limits",
+  },
+  {
+    id: 2,
+    alt: "people on a cog logo",
+    image: "/images/icon-onboarding.svg",
+    title: "Fast Onboarding",
+    message:
+      "We don’t do branches. Open your account in minutes online and start taking control of your finances right away.",
+  },
+  {
+    id: 3,
+    alt: "CPU with a brain in the middle",
+    image: "/images/icon-api.svg",
+    title: "Open Api",
+    message:
+      "Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier.",
+  },
+];
+
+export default info;
